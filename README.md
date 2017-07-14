7ff63e6c8f8db5db8ed1969dab99564a81a501bc
RicardoBrunetto <ra94182@uem.br>
2017-07-14 13:59:20 -0300

    Definições das estruturas

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Extensible-Hasing
 Trabalho 02 de Organização e Recuperação de Dados
+
+#To Do List
+- [ ] Definição das estruturas
+- [ ] Função de leitura de arquivo
+- [ ] Função de inserção de chaves
+  - [ ] Função de busca e verificação de chaves repetidas
---
+++ ehashing.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int BUCKET_SIZE;
+int Profundidade_Global;
+
+typedef struct{
+  int id;
+  int profundidade;
+  int chaves[BUCKET_SIZE];
+}Bucket;
+
+Bucket * diretorio;
+
+void read_input(FILE * f){
+  
+}
+
+int main(){
+
+  return 0;
+}
